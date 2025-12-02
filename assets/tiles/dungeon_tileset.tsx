64498021555fd580d7d6f52021e0ce74ce85406d
BurstingF <Jensiwaa@gmail.com>
2025-12-03 00:39:51 +0100

    v1.0.9

---
+++ assets/tiles/dungeon_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="battle_arena" tilewidth="16" tileheight="16" spacing="3" margin="3" tilecount="72" columns="9">
+ <image source="../images/tilemaps/dungeon_sprites.png" width="174" height="155"/>
+</tileset>
---
+++ assets/tiles/torch.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="torch" tilewidth="16" tileheight="16" tilecount="6" columns="6">
+ <image source="../images/tilemaps/torch.png" width="96" height="16"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="80"/>
+   <frame tileid="1" duration="80"/>
+   <frame tileid="2" duration="80"/>
+   <frame tileid="3" duration="80"/>
+   <frame tileid="4" duration="80"/>
+   <frame tileid="5" duration="80"/>
+  </animation>
+ </tile>
+</tileset>
